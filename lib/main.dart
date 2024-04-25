87f1461db3dad463aa1563a8887ecd3a7a5994d5
wjmals <w09jals7@naver.com>
2024-04-25 18:07:48 +0900

    FEAT: main 페이지 백엔드 연결 전 까지 햇음

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:cappp/login.dart';
-import 'package:cappp/navigation_page/chat.dart';
-import 'package:cappp/navigation_page/like.dart';
-import 'package:cappp/navigation_page/write.dart';
-import 'package:cappp/navigation_page/person.dart';
+import 'package:http/http.dart' as http;
+
+class Photo {
+  final String imageUrl;
+  final String description;
+
+  Photo({required this.imageUrl, required this.description});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      imageUrl: json['imageUrl'],
+      description: json['description'],
+    );
+  }
+}
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late TabController _nestedTabController;
+  late List<Photo> photos = []; // 사진 목록을 저장할 리스트
+
+  @override
+  // void initState() {
+  //   super.initState();
+  //   _nestedTabController = TabController(length: 5, vsync: this);
+  //   fetchPhotos(); // 이미지 가져오기 함수 호출
+  // }
+
+  // 이미지를 가져오는 함수
+  // void fetchPhotos() async {
+  //   final response = await http.get('YOUR_BACKEND_API_ENDPOINT');
+  //
+  //   if (response.statusCode == 200) {
+  //     final List<dynamic> data = json.decode(response.body);
+  //     setState(() {
+  //       photos = data.map((json) => Photo.fromJson(json)).toList();
+  //     });
+  //   } else {
+  //     throw Exception('Failed to load photos');
+  //   }
+  // }
+
+  @override
+  void dispose() {
+    _nestedTabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +74,63 @@ class MyApp extends StatelessWidget {
               onPressed: () {},
             ),
             IconButton(
-              onPressed: () {}, icon: Icon(
-              Icons.notifications_outlined,
-              color: Colors.black,
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications_outlined,
+                color: Colors.black,
+              ),
             ),
-            ),
-
           ],
+          bottom: TabBar(
+            controller: _nestedTabController,
+            indicatorColor: Colors.blue,
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.black54,
+            isScrollable: true,
+            tabs: <Widget>[
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3,
+                child: Tab(
+                  child: Text(
+                    "중고거래",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3,
+                child: Tab(
+                  child: Text(
+                    "공동구매",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
+            padding: EdgeInsets.only(top: 25.0),
+          ),
         ),
         body: Center(
-          child: Container(
-            child:ListView(),
-          ),
+          child: photos.isNotEmpty
+              ? ListView.builder(
+            itemCount: photos.length,
+            itemBuilder: (context, index) {
+              final photo = photos[index];
+              return ListTile(
+                title: Text('Photo ${index + 1}'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          PhotoDetailScreen(photo: photo),
+                    ),
+                  );
+                },
+              );
+            },
+          )
+              : CircularProgressIndicator(), // 이미지가 없는 경우 로딩 표시
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
@@ -69,10 +159,31 @@ class MyApp extends StatelessWidget {
           onTap: (int index) {},
           selectedItemColor: Colors.blue,
           unselectedItemColor: Colors.grey,
-          selectedFontSize: 0, // 선택된 아이템의 텍스트 크기를 0으로 설정
-          unselectedFontSize: 0, // 선택되지 않은 아이템의 텍스트 크기를 0으로 설정
-
+          selectedFontSize: 0,
+          unselectedFontSize: 0,
         ),
+      ),
+    );
+  }
+}
+
+class PhotoDetailScreen extends StatelessWidget {
+  final Photo photo;
+
+  const PhotoDetailScreen({Key? key, required this.photo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Photo Detail')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.network(photo.imageUrl),
+          SizedBox(height: 20),
+          Text(photo.description),
+        ],
       ),
     );
   }
---
+++ lib/search.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:cappp/main.dart';
+
+class search extends StatelessWidget {
+  const search({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+    );
+  }
+}
